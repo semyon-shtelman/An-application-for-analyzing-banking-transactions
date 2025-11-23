6318a5a411df554f832384ae612b7b8269d03ac2
Semyon Shtelman <shtelmaan@gmail.com>
2025-11-24 04:24:00 +0600

    add loads functions

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,19 @@
-
+import json
 import pandas as pd
 
-def load_transactions_excel(excel_file_patch: str) -> list[dict]:
+
+def load_transactions_excel(excel_file_patch: str) -> pd.DataFrame | list:
     try:
         df = pd.read_excel(excel_file_patch)
-        transactions = df.to_dict(orient='records')
-        return transactions
+        return df
     except FileNotFoundError:
-        print(f'error file {excel_file_patch} no find')
+        print(f"error file {excel_file_patch} no find")
         return []
     except Exception as e:
-        print(f'error read file Excel{e}')
+        print(f"error read file Excel{e}")
         return []
 
+
+def load_json_file(json_file_patch: str):
+    with open(json_file_patch, "r", encoding="utf-8") as json_file:
+        return json.load(json_file)
